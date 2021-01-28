6da4aa1cf623a985ba1460e5d011f608d3236fb6
chenlw <chenlw@bytter.com>
2021-01-28 12:44:45 +0800

    新增mockito工程

---
+++ spring-boot-mockito/src/main/java/com/chenlw/springboot/mockito/example/entity/SysUser.java
@@ -0,0 +1,252 @@
+package com.chenlw.springboot.mockito.example.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户对象 sys_user
+ *
+ * @author ruoyi
+ */
+@TableName("sys_user")
+public class SysUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户ID
+     */
+    @TableId(type = IdType.ASSIGN_UUID)
+    private String userId;
+
+    /**
+     * 部门ID
+     */
+    @TableField("dept_id")
+    private Long deptId;
+
+    /**
+     * 用户账号
+     */
+    @TableField("user_name")
+    private String userName;
+
+    /**
+     * 用户昵称
+     */
+    @TableField("nick_name")
+    private String nickName;
+
+    /**
+     * 用户邮箱
+     */
+    @TableField("email")
+    private String email;
+
+    /**
+     * 手机号码
+     */
+    @TableField("phonenumber")
+    private String phonenumber;
+
+    /**
+     * 用户性别
+     */
+    @TableField("sex")
+    private String sex;
+
+    /**
+     * 用户头像
+     */
+    @TableField("avatar")
+    private String avatar;
+
+    /**
+     * 密码
+     */
+    @TableField("password")
+    private String password;
+
+    /**
+     * 盐加密
+     */
+    @TableField(exist = false)
+    private String salt;
+
+    /**
+     * 帐号状态（0正常 1停用）
+     */
+    @TableField("status")
+    private String status;
+
+    /**
+     * 删除标志（0代表存在 2代表删除）
+     */
+    @TableField("del_flag")
+    private String delFlag;
+
+
+    /**
+     * 最后登陆IP
+     */
+    @TableField("login_ip")
+    private String loginIp;
+
+    /**
+     * 最后登陆时间
+     */
+    @TableField("login_date")
+    private Date loginDate;
+
+    /**
+     * 备注
+     */
+    @TableField("remark")
+    private String remark;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "SysUser{" +
+                "userId=" + userId +
+                ", deptId=" + deptId +
+                ", userName='" + userName + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", email='" + email + '\'' +
+                ", phonenumber='" + phonenumber + '\'' +
+                ", sex='" + sex + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", password='" + password + '\'' +
+                ", salt='" + salt + '\'' +
+                ", status='" + status + '\'' +
+                ", delFlag='" + delFlag + '\'' +
+                '}';
+    }
+}
---
+++ spring-boot-mockito/src/test/java/com/chenlw/springboot/mockito/example/dao/SysUserMapperTest.java
@@ -0,0 +1,42 @@
+package com.chenlw.springboot.mockito.example.dao;
+
+
+import com.chenlw.springboot.mockito.example.entity.SysUser;
+import com.chenlw.springboot.mockito.example.SpringBootMyBatisPlusApplication;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootMyBatisPlusApplication.class)
+public class SysUserMapperTest {
+
+    @Resource
+    SysUserMapper sysUserMapper;
+
+    @Test
+    public void selectAll() {
+        System.out.println(("----- selectAll method test ------"));
+        List<SysUser> userList = sysUserMapper.selectList(null);
+        userList.stream().forEach((sysUser -> {
+            System.out.println(sysUser);
+        }));
+    }
+
+    @Test
+    public void testInsert(){
+        SysUser sysUser = new SysUser();
+        sysUser.setNickName("chenlw");
+        sysUser.setUserName("chenlw");
+        sysUser.setPassword("1213213");
+        int result = sysUserMapper.insert(sysUser);
+        Assert.assertEquals(1,result);
+    }
+
+
+}
